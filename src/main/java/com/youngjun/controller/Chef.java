1fe388d50e940c796c35d3e0fc83adb8b686b5fc
SohnYoungJun <sohnyj37@gmail.com>
2020-03-18 05:09:54 +0900

    [Add] (Test)SampleTests Bean wiring, log4j2, Junit4 1. 스프링 프레임워크의 동작 테스트 2. new Restaurant() 를 통하여 객체를 직접 생성하지 않고 객체가 생성되었다. 3. @Data 를 이용하여 Lombok으로 getter/setter를 자동으로 생성하였고, onMethod 속성을 이용하여 작성된 setter에 @Autowired 로 bean-wiring

---
+++ src/main/java/com/youngjun/controller/Chef.java
@@ -0,0 +1,9 @@
+package com.youngjun.controller;
+
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
+@Component
+@Data
+public class Chef {
+}
---
+++ src/test/java/com/youngjun/sample/SampleTests.java
@@ -0,0 +1,30 @@
+package com.youngjun.sample;
+import com.youngjun.controller.Restaurant;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j2;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.assertNotNull;
+
+//@ExtendWith(SpringExtension.class)//Junit4에서 @RunWith(SpringJunit4ClassRunner.class)
+@RunWith(SpringJUnit4ClassRunner.class)
+@Log4j2
+@ContextConfiguration("file:web/WEB-INF/applicationContext.xml")
+public class SampleTests {
+
+    @Setter(onMethod_ = { @Autowired }) //자동 빈 묶기를 합니다.(lombok)
+    private Restaurant restaurant;
+
+    @Test
+    public void testExist(){
+        assertNotNull(restaurant);
+        log.info(restaurant);
+        log.info("---------------------");
+        log.info(restaurant.getChef());
+    }//testExist()
+}//SampleTests
