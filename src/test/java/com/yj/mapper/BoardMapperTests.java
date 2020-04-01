97fd79c6048ac4cbd6b85515a6c4fc6cc42ef40e
SohnYoungJun <sohnyj37@gmail.com>
2020-04-01 23:16:24 +0900

    [Update] Create 구문 작성 * 현재 Invalid bound statement 발생, 원인 파악 중

--- src/test/java/com/yj/mapper/BoardMapperTests.java
+++ src/test/java/com/yj/mapper/BoardMapperTests.java
@@ -1,5 +1,6 @@
 package com.yj.mapper;
 
+import com.yj.domain.BoardVO;
 import lombok.Setter;
 import lombok.extern.log4j.Log4j2;
 import org.junit.Test;
@@ -19,4 +20,16 @@ public class BoardMapperTests {
     public void testGetList(){
         mapper.getList().forEach(board ->log.info(board) );
     };
+
+    @Test
+    public void testInsert(){
+        BoardVO board = new BoardVO();
+        board.setTitle("새로 작성하는 글");
+        board.setContent("새로 작성하는 내용");
+        board.setWriter("newbie");
+        mapper.insert(board);
+        log.info(board);
+
+    }//testInsert()
+
 }//BoardMapperTests()
