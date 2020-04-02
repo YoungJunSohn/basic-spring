e14bc1d47f7d8bb87de8f0c70191d4fe7fd8b4a1
SohnYoungJun <sohnyj37@gmail.com>
2020-04-02 18:38:36 +0900

    [Update] CRUD TEST * mapper interface에 메서드를 추가하였습니다. * mapper.xml에 sql 구문을 추가하였습니다. * 테스트 클래스 작성 및 실행으로 작동 검사를 하였습니다.

--- src/main/java/com/yj/mapper/BoardMapper.java
+++ src/main/java/com/yj/mapper/BoardMapper.java
@@ -1,14 +1,21 @@
 package com.yj.mapper;
 
 import com.yj.domain.BoardVO;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
 public interface BoardMapper {
-    @Select("select * from tbl_board where bno > 0")
+    //@Select("select * from tbl_board where bno > 0")
     public List<BoardVO> getList();
 
     public void insert(BoardVO board);
     public void insertSelectKey(BoardVO board);
+
+    public BoardVO read(Long bno);
+
+    public int delete(Long no);
+
+    public int update(BoardVO board);
 }//BoardMapper
--- src/test/java/com/yj/mapper/BoardMapperTests.java
+++ src/test/java/com/yj/mapper/BoardMapperTests.java
@@ -27,9 +27,43 @@ public class BoardMapperTests {
         board.setTitle("새로 작성하는 글");
         board.setContent("새로 작성하는 내용");
         board.setWriter("newbie");
-        mapper.insert(board);
+        //mapper.insert(board);
+        mapper.insertSelectKey(board);
         log.info(board);
-
     }//testInsert()
 
+    @Test
+    public void testRead(){
+        //게시물 번호로 불러오는 게시물 정보
+        //warning! -- 게시물 번호가 존재하는지 반드시 그 여부를 확인해야 합니다.
+        BoardVO board = mapper.read(5L);
+        log.info(board);
+    }//testRead()
+
+    @Test
+    public void testDelete(){
+        log.info("Delete 구문이 실행되었습니다 : " +mapper.delete(3L));
+    }//testDelete()
+
+    @Test
+    public void testUpdate(){
+        Long num = 5L;
+        BoardVO board = new BoardVO();//데이터를 객체형식으로 넘기기위해서 생성합니다.
+        board.setTitle("수정된 제목");
+        board.setContent("수정된 내용");
+        board.setWriter("user01");
+        //실행전 해당 글 번호가 데이터베이스에 존재하는지 확인해야합니다.
+        board.setBno(num);
+
+        int count = mapper.update(board);
+        log.info("Update 구문 실행 : "+num+"번 글을 수정합니다. ->"+count);
+    }//testUpdate()
 }//BoardMapperTests()
+
+/*
+* 작동 순서
+* 1. mapper interface 에서 메서드를 만듭니다.
+* 2. mapper.xml에서 sql 구문을 작성합니다.
+* 3. 작성된 Context를 Spring에서 읽을 수 있도록 applicationContext에 bean 등록합니다.(context:component-scan)
+* 4. 오류가 없는지 테스트 클래스에서 작동시킵니다.
+* */
